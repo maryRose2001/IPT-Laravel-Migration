5a7e7918ea3d263b41f2968179195e4ae2675464
maryRose2001 <montebon.maryarosas@gmail.com>
2022-09-19 23:07:45 +0800

    initial

---
+++ resources/views/about.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Prelim Project</title>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+<body>
+    <div class="text-center">
+    </nav class="nav justify-content-center>
+           <a href="{{url('/')}}" class="nav-link">Home</a>
+           <a href="{{url('/about')}}" class="nav-link">About Us</a>
+           <a href="{{url('/profilr')}}" class="nav-link">Company Profile</a>
+         </nav>
+        <h1>About Us</h1>
+        
+        <div class="card shadow-sm style="width: 500px">
+            <div class="card-body">
+                <div class="card-title">Our Company</div>
+            </p>
+               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas saepe vel quae voluptatibus delectus eligendi dolore, iusto amet quis eum porro! Natus, sit ratione atque recusandae in tempore provident quae.
+            </p>
+            </div>
+        </div>
+
+       
+    </div>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Prelim Project</title>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+<body>
+    <div class="text-center">
+        </nav class="nav justify-content-center>
+           <a href="{{url('/')}}" class="nav-link">Home</a>
+           <a href="{{url('/about')}}" class="nav-link">About Us</a>
+           <a href="{{url('/profilr')}}" class="nav-link">Company Profile</a>
+         </nav>
+           <h1>Prelim Project</h1>
+        <div class="card shadow-sm style="width: 500px">
+            <div class="card-body">
+                <div class="card-title">Welcome Message</div>
+            </p>
+                This is just a test emergency broadcast system.
+            </p>
+            </div>
+        </div>
+
+       
+    </div>
+</body>
+</html>
